d81fc3547a38a703b6c31e155528fa3fa5bb03b9
Leandro Rosa <leandrogr03@gmail.com>
2021-10-18 10:57:07 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1 className="text-lg text-blue-600">CHAT APP</h1>
+    </div>
+  )
+}
+
+export default Home
